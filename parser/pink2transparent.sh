75f621af75846d367d2da1ccde73ef0c9e65d9d0
Rachel Fish <itsrachelfish@gmail.com>
2014-05-28 23:52:30 -0600

    Learning bash, learning bash~

--- parser/pink2transparent.sh
+++ parser/pink2transparent.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-convert test2.bmp -fuzz "2%" -transparent \#ff00ff test2.png
+cd src/item/
+
+for img in *.bmp; do
+    filename=${img%.*}
+    base64=$(echo $filename | base64)
+    convert "$filename.bmp" -fuzz "2%" -transparent \#ff00ff "$base64.png"
+done
+
+#convert test2.bmp -fuzz "2%" -transparent \#ff00ff test2.png
+#convert test2.bmp -fuzz "2%" -transparent \#ff00ff test2.png
