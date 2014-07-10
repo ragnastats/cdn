ce792674698a16323f2e67d3051d59bf88394f71
Rachel Fish <itsrachelfish@gmail.com>
2014-07-10 00:01:59 -0600

    Refactored parser script to accept dir as argument

--- parser/pink2transparent.sh
+++ parser/pink2transparent.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 path=$(pwd)
-cd $path/src/item/
+source=$1
+cd $path/src/$source/
 
 for img in *.bmp; do
     filename=${img%.*}
- #   base64=$(echo $filename| base64 | tr / -)
-    convert "$filename.bmp" -fuzz "2%" -transparent \#ff00ff "$path/../item/src/$filename.png"
+    convert "$filename.bmp" -fuzz "2%" -transparent \#ff00ff "$path/../$source/src/$filename.png"
 done
 
-cd $path/src/collection/
-
-for img in *.bmp; do
-    filename=${img%.*}
-  #  base64=$(echo $filename| base64 | tr / -)
-    convert "$filename.bmp" -fuzz "2%" -transparent \#ff00ff "$path/../collection/src/$filename.png"
-done
