7b7b61b035ca8e5930eba3cd66e883589f6d8919
Rachel Fish <itsrachelfish@gmail.com>
2014-07-20 15:44:15 -0600

    Updated pink2transparent to allow trimming transparency off edges

--- parser/pink2transparent.sh
+++ parser/pink2transparent.sh
@@ -2,10 +2,15 @@
 
 path=$(pwd)
 source=$1
+trim=$2
 cd $path/src/$source/
 
 for img in *.bmp; do
     filename=${img%.*}
-    convert "$filename.bmp" -fuzz "2%" -transparent \#ff00ff "$path/../$source/src/$filename.png"
+
+    if [ "$trim" == "trim" ]
+        then echo "Processing $filename (trimmed)"; convert "$filename.bmp" -bordercolor \#ff00ff -border 1 -fuzz "2%" -trim -transparent \#ff00ff "$path/../$source/src/$filename.png"
+        else echo "Processing $filename"; convert "$filename.bmp" -fuzz "2%" -transparent \#ff00ff "$path/../$source/src/$filename.png"
+    fi
 done
 
